a0a79a246cd3db7d7ab5f39895b34ac48a6efd83
anso <asod@itu.dk>
2022-03-11 12:43:39 +0100

    d

--- test.js
+++ test.js
@@ -7,16 +7,9 @@ function highlight(e) {
 }
 
 function fnselect(){
-
-    alert($("tr.selected td:first" ).html());
-}
-
 $("#myTable tr").click(function(){
     $(this).addClass('selected').siblings().removeClass('selected');    
     var value=$(this).find('td:first').html();
     alert(value);    
  });
- 
- $('.ok').on('click', function(e){
-     alert($("#table tr.selected td:first").html());
- });
+}
