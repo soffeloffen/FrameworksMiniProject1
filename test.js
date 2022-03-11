2a3417e7764b554e268b318571fecd69a389a674
anso <asod@itu.dk>
2022-03-11 12:46:10 +0100

    d

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
-var table = document.getElementById('table'),
+var table = document.getElementById('myTable'),
     selected = table.getElementsByClassName('selected');
 table.onclick = highlight;
 function highlight(e) {
     if (selected[0]) selected[0].className = '';
     e.target.parentNode.className = 'selected';
 }
-
-function my_func() {
-    my_func_1();
-    my_func_2();
-}
-
 function fnselect(){
-$("#myTable tr").click(function(){
-    $(this).addClass('selected').siblings().removeClass('selected');    
-    var value=$(this).find('td:first').html();
-    alert(value);    
- });
-}
 
+    $("#myTable tr").click(function(){
+        $(this).addClass('selected').siblings().removeClass('selected');    
+        var value=$(this).find('td:first').html();
+        alert(value);    
+     });
+     
+     $('.ok').on('click', function(e){
+         alert($("#table tr.selected td:first").html());
+     });
+}
