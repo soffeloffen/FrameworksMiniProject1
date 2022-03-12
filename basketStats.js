d73572b8efc9896b17abc4a28fdc43fc9838da7b
anso <asod@itu.dk>
2022-03-12 11:44:59 +0100

    l

--- basketStats.js
+++ basketStats.js
@@ -1,11 +1,15 @@
 var amount= 0;
 
 function setDrug1(){
-sessionStorage.setItem( "ItemName", "Magic Mushrooms" );
-sessionStorage.setItem( "Amount", amount=amount +1);
-
+sessionStorage.setItem( "Drug1Name", "Magic Mushrooms" );
+sessionStorage.setItem( "Drug1Amount", 1);
 }
 
+function updateDrug1(){
+var total = parseInt(sessionStorage.getItem( "Drug1Amount" ) );
+var updatedTotal = total +1;
+sessionStorage.setItem( "Drug1Amount", updatedTotal ); 
+}
 
 
 
